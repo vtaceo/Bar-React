f0a683fe5e904565039b9b8b151f50e626eb71c5
vtaceo <87086639+vtaceo@users.noreply.github.com>
2023-05-05 13:59:48 -0500

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,37 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import {
+    Inter
+} from '@next/font/google';
+import styles from '@/styles/Home.module.css';
+import Featured from '@/components/Featured';
+import Navbar from '@/components/Navbar';
+
+const inter = Inter({
+    subsets: ['latin']
+})
+
+export default function Home() {
+    return ( <
+        div className = {
+            styles.container
+        } >
+        <
+        Head >
+        <
+        title > Balones Sports Bar & Billar < /title> <
+        meta name = "description"
+        content = "Bar in DFW" / >
+        <
+        meta name = "viewport"
+        content = "width=device-width, initial-scale=1" / >
+        <
+        link rel = "icon"
+        href = "/favicon.ico" / >
+        <
+        /Head> {
+            /* <Navbar></Navbar> */ } <
+        Featured > < /Featured> <
+        /div>
+    )
+}
